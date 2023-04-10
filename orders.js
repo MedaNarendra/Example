bd82d3eb876d65690795a1c3797e538df6b5f706
Meda Narendra <60517987+MedaNarendra@users.noreply.github.com>
2023-04-10 13:11:19 +0530

    Version 0.0.0

---
+++ orders.js
@@ -0,0 +1,22 @@
+fetch("https://5fc1a1c9cb4d020016fe6b07.mockapi.io/api/v1/orders").then((data)=>{
+    console.log(data);
+    return data.json();
+}).then((objectData)=>{
+    console.log(objectData);
+    let tableData="";
+    objectData.map((values)=>{
+        tableData +=`
+        <tr>
+        <td>${values.id}</td>
+        <td>${values.customerName}</td>
+        <td>${values.orderDate}</td>
+        <td>${values.amount}</td>
+        <td>${values.orderStatus}</td>
+        </tr>`;
+    });
+    document.getElementById("tbody").innerHTML=tableData;
+});
+function logout(){
+    window.location.assign("login.html");
+};
+
